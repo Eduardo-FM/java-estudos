1a01d4a43e2594bb30fd371fe0364a0cbc825a0c
Eduardo Freitas Machado <eduardo.freitas.machado@gmail.com>
2024-06-05 17:03:27 -0300

    resolvendo exercicios lista

---
+++ src/Lists/OperacoesBasicas/Item.java
@@ -0,0 +1,36 @@
+package Lists.OperacoesBasicas;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private String nome;
+    private double  preco;
+    private Integer quantidade;
+
+    public Item(String nome, double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
